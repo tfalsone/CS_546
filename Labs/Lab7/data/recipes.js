4dcb28fb03288b52eac6d460f8efd0f6ec1803a4
Thomas Falsone <thomas.falsone@gmail.com>
2018-03-08 22:51:58 -0500

    Finish lab 7

--- Labs/Lab7/data/recipes.js
+++ Labs/Lab7/data/recipes.js
@@ -3,25 +3,26 @@ const recipes = mongoCollections.recipes;
 const uuid = require("node-uuid");
 
 const exportedMethods = {
-    // GET ALL
+    // GET ALL (verified works)
     async getAllRecipes() {
         const recipeCollection = await recipes();
         return await recipeCollection.find({}).toArray();
     },
-    // GET ONE
+    // GET ONE (verified works)
     async getRecipeById(id) {
         const recipeCollection = await recipes();
         const recipe = await recipeCollection.findOne({_id: id});
 
         if(!recipe) throw "Recipe not found";
+        console.log(recipe);
         return recipe;
     },
-    // POST
+    // POST (gives undefined values)
     async addRecipe(title, ingredients, steps) {
         // Error checking
-        //if (typeof title !== "string") throw "No title provided";
-        //if (!Array.isArray(ingredients)) throw "No ingredients provided";
-        //if (!Array.isArray(steps)) throw "No steps provided";
+        // if (typeof title !== "string") throw "No title provided";
+        // if (!Array.isArray(ingredients)) throw "No ingredients provided";
+        // if (!Array.isArray(steps)) throw "No steps provided";
 
         console.log(title);
 
@@ -30,39 +31,55 @@ const exportedMethods = {
             _id: uuid.v4(),
             title: title,
             ingredients: ingredients,
-            steps: steps
+            steps: steps,
         };
 
         const newRecipeInfo = await recipeCollection.insertOne(newRecipe);
-        const newId = newRecipe._id;
+        const newId = newRecipeInfo.insertedId;
         return await this.getRecipeById(newId);
     },
-    // DELETE
+    // DELETE (deletes item, but never finished loading in postman)
     async deleteRecipe(id) {
+        if (!id) throw "You must provide a valid recipe id";
+
         const recipeCollection = await recipes();
         const deletionInfo = await recipeCollection.removeOne({_id: id});
         if (deletionInfo.deletedCount === 0) {
             throw `Could not delete post with id of ${id}`;
         }
-        return deletionInfo;
+        // console.log("Recipe deleted");
     },
     // PATCH
     async updateRecipe(id, updatedRecipe) {
+        if (!id) throw "You must provide a valid recipe id";
+
         const recipeCollection = await recipes();
 
         const updatedRecipeData = {};
 
         if (updatedRecipe.title) {
-            updatedRecipeData.title = updatedRecipe.title;
+            if (typeof updatedRecipe.title !== "string") throw "No title provided";
+            console.log(`Patching title with ${updatedRecipe.title}`);
+            // updatedRecipeData.title = updatedRecipe.title;
+            updatedRecipeData["title"] = updatedRecipe.title;
         }
 
         if (updatedRecipe.ingredients) {
-            updatedRecipeData.ingredients = updatedRecipe.ingredients;
+            if (!Array.isArray(updatedRecipe.ingredients)) throw "No ingredients provided";
+            console.log(`Patching ingredients with ${updatedRecipe.ingredients}`);
+            // updatedRecipeData.ingredients = updatedRecipe.ingredients;
+            updatedRecipeData["ingredients"] = updatedRecipe.ingredients;
         }
 
         if (updatedRecipe.steps) {
-            updatedRecipeData.steps = updatedRecipe.steps;
+            if (!Array.isArray(updatedRecipe.steps)) throw "No steps provided";
+            console.log(`Patching steps with ${updatedRecipe.steps}`);
+            // updatedRecipeData.steps = updatedRecipe.steps;
+            updatedRecipeData["steps"] = updatedRecipe.steps;
         }
+
+        console.log("The new recipe data: ");
+        console.log(updatedRecipeData);
 
         let updateCommand = {
             $set: updatedRecipeData
@@ -75,27 +92,25 @@ const exportedMethods = {
         return await this.getRecipeById(id);
     },
     // PUT
-    async newRecipeWithId(id, newRecipeDetails) {
-        if (typeof title != "string") throw "No title provided";
-        // Need more error checking?
-        
+    async putRecipe(id, newRecipe) {
+        if (!id) throw "You must provide a valid recipe id";
+        if (typeof newRecipe.title !== "string") throw "No title provided";
+        if (!Array.isArray(newRecipe.ingredients)) throw "No ingredients provided";
+        if (!Array.isArray(newRecipe.steps)) throw "No steps provided";
+
         const recipeCollection = await recipes();
-        const newRecipe = {
+
+        const updatedRecipeData = {
             _id: id,
-            title: newRecipeDetails.title,
-            ingredients: newRecipeDetails.ingredients,
-            steps: newRecipeDetails.steps
+            title: newRecipe.title,
+            ingredients: newRecipe.ingredients,
+            steps: newRecipe.steps
         };
 
-        const newInsertInformation = await recipeCollection.insertOne(newRecipe);
-        const newId = newInsertInformation._id; // is insertedId correct?
-        return await this.getRecipeById(newId);
-    },
-    async putRecipe(id, newRecipe) {
-        const recipeCollection = await recipes();
-        this.deleteRecipe(id);
+        const updatedInfo = await recipeCollection.replaceOne({_id:id}, updatedRecipeData);
+        if (updatedInfo.modifiedCount === 0) throw "Unable to update recipe";
         
-        return await this.newRecipeWithId(id, newRecipe);
+        return await this.getRecipeById(id, newRecipe);
     }
 };
 
--- Labs/Lab7/routes/recipes.js
+++ Labs/Lab7/routes/recipes.js
@@ -23,7 +23,6 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    console.log(req);
     const recipePostData = req.body;
     // console.log(recipePostData);
     try {
@@ -47,6 +46,7 @@ router.put("/:id", async (req, res) => {
 
     try {
         const updatedRecipe = await recipeData.putRecipe(req.params.id, updatedData);
+        res.json("Recipe put");
         res.json(updatedRecipe);
     } catch (e) {
         res.status(500).json({error: e});
@@ -62,6 +62,8 @@ router.delete("/:id", async (req, res) => {
 
     try {
         await recipeData.deleteRecipe(req.params.id);
+        console.log("Recipe deleted");
+        res.send("Recipe deleted");
     } catch (e) {
         res.status(500).json({error: e});
     }
@@ -76,7 +78,10 @@ router.patch("/:id", async (req, res) => {
     }
 
     try {
-        await recipeData.updateRecipe(req.params.id, updatedData);
+        const updatedRecipe = await recipeData.updateRecipe(req.params.id, updatedData);
+        
+        res.send("Recipe patched");
+        res.json(updatedRecipe);
     } catch (e) {
         res.status(500).json({error: e});
     }
